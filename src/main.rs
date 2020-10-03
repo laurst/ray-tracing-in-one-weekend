11b6b4642a74d23fbbc8984711698acc9d24c5b3
Laurent Sebellin <laurent.sebellin@stylight.com>
2020-10-03 15:02:28 +0200

    first

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod vec3;
+
+const WIDTH: i32 = 256;
+const HEIGHT: i32 = 256;
+
+fn main() {
+    println!("P3\n{} {}\n255", WIDTH, HEIGHT);
+
+    for j in (0..HEIGHT).rev() {
+        eprint!("\rlines remaining : {}", j);
+        for i in 0..WIDTH {
+            let color = vec3::Vec3{
+                x: i as f64 / (WIDTH-1) as f64,
+                y: j as f64 / (HEIGHT-1) as f64,
+                z: 0.25,
+            };
+            vec3::write_color(&color);
+        }
+    }
+    eprintln!("\ndone");
+}
---
+++ src/vec3.rs
@@ -0,0 +1,128 @@
+use std::ops;
+
+#[derive(Debug)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+// TODO : should probably use default trait impl in order to define both color and point
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3{x, y, z}
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x.powi(2) + self.y.powi(2) + self.z.powi(2)
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn dot(&self, other: &Self) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        Vec3{
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    pub fn unit_vector(&self) -> Self {
+        let len = self.length();
+        Vec3{
+            x: self.x / len,
+            y: self.y / len,
+            z: self.z / len,
+        }
+    }
+
+}
+
+impl ops::Add for Vec3 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Vec3{
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Vec3{
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+// TODO missing constant mul
+impl ops::Mul for Vec3 {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        Vec3{
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+impl ops::Div for Vec3 {
+    type Output = Self;
+    fn div(self, other: Self) -> Self {
+        Vec3{
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
+    }
+}
+
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, other: f64) {
+        *self = Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+
+pub fn write_color(v: &Vec3) {
+    println!("{} {} {}",
+             (255.999 * v.x) as u32,
+             (255.999 * v.y) as u32,
+             (255.999 * v.z) as u32
+    );
+}
