740564afe0a2af3093fbe87eef3791f17885c590
Laurent Sebellin <laurent.sebellin@stylight.com>
2020-10-28 09:44:15 +0100

    circle

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use std::io::{stderr, Write};
 use vec3::{Point3, Vec3, Color};
 
 fn ray_color(ray: &ray::Ray) -> Color {
+    let camera = Point3::new(0., 0., -1.);
+    if hit_sphere(&camera, 0.5, ray) {
+        return Color::new(1., 0., 0.);
+    }
+
     let unit_direction = ray.dir.unit_vector();
     let t = 0.5 * (unit_direction.y + 1.0);
 
@@ -12,8 +17,17 @@ fn ray_color(ray: &ray::Ray) -> Color {
            + Color::new(0.5, 0.7, 1.0).mul(t);
 }
 
+fn hit_sphere(center: &Point3, radius: f64, r: &ray::Ray) -> bool {
+    let oc = r.orig - *center;
+    let a = r.dir.dot(&r.dir);
+    let b = 2.0 * oc.dot(&r.dir);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.
+}
+
 fn main() {
-    // TODO : find and fix x-y inversion ?
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
